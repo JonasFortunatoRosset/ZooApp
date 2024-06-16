13d37a2ba7cee671f878acd4d983861279ce961d
JonasFortunatoRosset <jonasfortunatorosset@gmail.com>
2024-06-16 12:17:08 -0300

    usuarios get

--- controllers/usuariosController.py
+++ controllers/usuariosController.py
@@ -12,3 +12,8 @@ def usuariosController():
             return 'Usuario inserido com sucesso', 200
         except Exception as e:
             return {'error: Erro ao cadastrar usuário. Erro: {}'.format(str(e))}, 400
+    elif request.method == 'GET':
+        try: 
+            data = Usuarios.query.all()
+        except Exception as e:
+            return 'Não foi possível buscar os usuários', 405
